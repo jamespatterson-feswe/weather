3259a892301e4516eab8635f93df1830e46924d1
James Patterson <jamespatterson.swe@gmail.com>
2026-03-07 13:59:15 -0500

    build: query client, setup, test getWeather method

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 // import reactLogo from './assets/react.svg'
 
+import { useQuery } from "@tanstack/react-query"
+import { getWeather } from "./service/weather.service"
+
 function App() {
+  const { data } = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({lat: 50, long: 50})
+  });
+
   return (
-    <div className="size-32 bg-red-500">HELLO WORLD</div>
+    <div>{ JSON.stringify(data) }</div>
   )
 }
 
